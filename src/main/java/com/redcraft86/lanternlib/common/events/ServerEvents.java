f97e79606870fffbafbbb00609ae8e883ef19daf
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-01-16 22:20:57 -0500

    Port mob griefing blacklist

---
+++ src/main/java/com/redcraft86/lanternlib/common/events/ServerEvents.java
@@ -0,0 +1,29 @@
+package com.redcraft86.lanternlib.common.events;
+
+import com.redcraft86.lanternlib.LanternLib;
+import com.redcraft86.lanternlib.configs.CommonCfg;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityMobGriefingEvent;
+
+@EventBusSubscriber(modid = LanternLib.MOD_ID)
+public class ServerEvents {
+
+    @SubscribeEvent
+    static void onMobGriefing(EntityMobGriefingEvent event) {
+        Entity entity = event.getEntity();
+        if (entity.level().isClientSide()) {
+            return;
+        }
+
+        ResourceLocation resource = BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType());
+        if (CommonCfg.GRIEFING_BLACKLIST.get().contains(resource.toString())) {
+            event.setCanGrief(false);
+        }
+    }
+}
--- src/main/java/com/redcraft86/lanternlib/configs/CommonCfg.java
+++ src/main/java/com/redcraft86/lanternlib/configs/CommonCfg.java
@@ -1,9 +1,20 @@
 package com.redcraft86.lanternlib.configs;
 
+import java.util.List;
+
+import com.redcraft86.lanternlib.util.ValidationUtils;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class CommonCfg {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    public static final ModConfigSpec.ConfigValue<List<? extends String>> GRIEFING_BLACKLIST;
+
+    static {
+        GRIEFING_BLACKLIST = BUILDER.defineListAllowEmpty("griefingBlacklist",
+                List.of("minecraft:enderman", "minecraft:fireball"), () -> "minecraft:",
+                ValidationUtils::isResourceLoc);
+    }
 
     public static final ModConfigSpec SPEC = BUILDER.build();
 
