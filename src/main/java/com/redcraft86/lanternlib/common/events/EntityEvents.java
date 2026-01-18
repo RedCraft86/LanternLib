55c7210b20d7b1ed32b13b51b3abddfd3168537b
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-01-18 18:55:47 -0500

    rename

--- src/main/java/com/redcraft86/lanternlib/common/events/EntityEvents.java
+++ src/main/java/com/redcraft86/lanternlib/common/events/EntityEvents.java
@@ -12,7 +12,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityMobGriefingEvent;
 
 @EventBusSubscriber(modid = LanternLib.MOD_ID)
-public class ServerEvents {
+public class EntityEvents {
 
     @SubscribeEvent
     static void onMobGriefing(EntityMobGriefingEvent event) {
