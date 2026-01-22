90a67d2754cc57f484c628c6a9454bd8bccbd68b
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-01-21 20:05:49 -0500

    capitalize

--- src/main/java/com/redcraft86/lanternlib/TransientFlags.java
+++ src/main/java/com/redcraft86/lanternlib/TransientFlags.java
@@ -5,35 +5,35 @@ import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TransientFlags {
-    private static final Set<String> flags = ConcurrentHashMap.newKeySet();
+    private static final Set<String> FLAGS = ConcurrentHashMap.newKeySet();
 
     public static void addFlag(String flag) {
         validateFlag(flag);
-        flags.add(flag);
+        FLAGS.add(flag);
     }
 
     public static void removeFlag(String flag) {
         validateFlag(flag);
-        flags.remove(flag);
+        FLAGS.remove(flag);
     }
 
     public static boolean hasFlag(String flag) {
         validateFlag(flag);
-        return flags.contains(flag);
+        return FLAGS.contains(flag);
     }
 
     public static boolean toggleFlag(String flag) {
         validateFlag(flag);
-        if (flags.remove(flag)) {
+        if (FLAGS.remove(flag)) {
             return false;
         } else {
-            flags.add(flag);
+            FLAGS.add(flag);
             return true;
         }
     }
 
     public static Set<String> getFlags() {
-        return Collections.unmodifiableSet(flags);
+        return Collections.unmodifiableSet(FLAGS);
     }
 
     private static void validateFlag(String flag) {
