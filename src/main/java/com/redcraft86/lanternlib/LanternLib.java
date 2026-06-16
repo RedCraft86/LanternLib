1d2e850016a5b4e542e9c6a49b8b20077ad28f14
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-06-15 22:31:13 -0400

    Initial commit

---
+++ src/main/java/com/redcraft86/lanternlib/LanternLib.java
@@ -0,0 +1,21 @@
+package com.redcraft86.lanternlib;
+
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.IEventBus;
+
+@Mod(LanternLib.MOD_ID)
+public final class LanternLib {
+    public static final String MOD_ID = "lanternlib";
+
+    public LanternLib(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
